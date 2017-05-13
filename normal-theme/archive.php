fe875a1357f0f44a19da43d08b52ef12c1a5b488
codekipple <codekipple@gmail.com>
2017-05-13 17:56:38 +0100

    archive.php in normal-theme now more simple

--- normal-theme/archive.php
+++ normal-theme/archive.php
@@ -1,45 +1,21 @@
-<?php
-/**
- * The template for displaying Archive pages.
- *
- * Used to display archive-type pages if nothing more specific matches a query.
- * For example, puts together date-based pages if no date.php file exists.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- */
+<?php get_header(); ?>
 
-get_header(); ?>
+<div class="main-content">
+    <div class="main-content">
+    <?php if ( have_posts() ) : ?>
+        <h1><?php the_archive_title(); ?></h1>
+        <?php the_archive_description(); ?>
 
-    <?php
-    if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile; ?>
 
-        <header class="page-header">
-            <?php
-                the_archive_title( '<h1 class="page-title">', '</h1>' );
-                the_archive_description( '<div class="archive-description">', '</div>' );
-            ?>
-        </header><!-- .page-header -->
-
-        <?php
-        /* Start the Loop */
-        while ( have_posts() ) : the_post();
-
-            /*
-             * Include the Post-Format-specific template for the content.
-             * If you want to override this in a child theme, then include a file
-             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-             */
-            get_template_part( 'template-parts/content', get_post_format() );
-
-        endwhile;
-
-        the_posts_navigation();
-
-    else :
-
-        get_template_part( 'template-parts/content', 'none' );
-
-    endif; ?>
+        <?php the_posts_navigation(); ?>
+    <?php else: ?>
+        <p>No posts.</p>
+    <?php endif; ?>
+    </div>
+</div>
 
 <?php
 get_sidebar();
